c33c1681fd17de17b35754b1c73ecf923559f4ce
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-09 15:11:26 -0400

    add item model, validations, and relationships, migrate db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
   validates :username, uniqueness: true
+
+  has_many :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_143415) do
+ActiveRecord::Schema.define(version: 2020_06_09_190939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_06_09_143415) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "users"
 end
