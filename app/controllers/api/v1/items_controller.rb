40cdd5abf9bcaa58bfbf56b538b15c55161a800d
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-09 15:22:31 -0400

    add index route for items nested under users

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::ItemsController < ApplicationController
+  # GET /users/:id/items
+  def index
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Stuart", password: "password")
-User.create(username: "Jenni", password: "password")
+# User.create(username: "Stuart", password: "password")
+# User.create(username: "Jenni", password: "password")
+
+s = User.first
+s.items.create(name: "Laptop")
+s.items.create(name: "Peloton")
+s.items.create(name: "Mini Fridge")
+
+j = User.second
+s.items.create(name: "Laptop")
+s.items.create(name: "Bookshelves")
+s.items.create(name: "Books")
