5a37517f8b8e93b2ded0bef09b7bfd891d1faef7
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-09 12:12:41 -0400

    update users controller to provide better errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,9 +18,9 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: UserSerializer.new(@user), status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: @user.errors.full_messages.to_sentence }, status: :unprocessable_entity
     end
   end
 
@@ -46,6 +46,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation)
+      params.require(:user).permit(:username, :password)
     end
 end
