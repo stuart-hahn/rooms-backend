778a144393555f82fb9a720b0e805b22ea618f45
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-09 15:49:37 -0400

    add index action to items controller, update serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :set_user
   # GET /users/:id/items
   def index
+    if @user
+      @items = @user.items
+      render json: ItemSerializer.new(@items), status: :ok
+    else
+      render json: { error: "User not found." }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def item_params
+    params.require(:item).permit(:name, :user_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(username: "Stuart", password: "password")
-# User.create(username: "Jenni", password: "password")
+User.create(username: "stuart", password: "password")
+User.create(username: "jenni", password: "password")
 
 s = User.first
 s.items.create(name: "Laptop")
@@ -15,6 +15,6 @@ s.items.create(name: "Peloton")
 s.items.create(name: "Mini Fridge")
 
 j = User.second
-s.items.create(name: "Laptop")
-s.items.create(name: "Bookshelves")
-s.items.create(name: "Books")
+j.items.create(name: "Laptop")
+j.items.create(name: "Bookshelves")
+j.items.create(name: "Books")
